cccb699c3828c2156d4ba76e8cfac8dd249395b6
Ian Beihl <ianbeihl@gmail.com>
2018-06-11 11:33:17 -0700

    to infinity

--- day1.js
+++ day1.js
@@ -9,12 +9,7 @@ const whoAmI  = function(name, age){
     if ((!name) || (!age)){
         console.error('Arguments not valid');
     }
-<<<<<<< HEAD
-    else if (typeof age !== "number"){
-        throw new Error("age is not a number")
-    }
-=======
->>>>>>> b20a74a7635580b4edfe844107d4c759096447b3
+
     else if (age < 0){
         throw new Error("Age cant be negative");
     }
@@ -27,14 +22,9 @@ const whoAmI  = function(name, age){
     console.log(`I was born in ${yob}`)
     }
 }
-<<<<<<< HEAD
+
 let age = 35;
 let name = null;
 
 whoAmI ("bob", 35);
-=======
 
-let name = 'Brandon';
-let age = 45;
-whoAmI (name, age);
->>>>>>> b20a74a7635580b4edfe844107d4c759096447b3
--- day1Set2.js
+++ day1Set2.js
@@ -9,3 +9,20 @@ let firstName = 'Beyonce';
 let lastName = 'Knowles';
 
 console.log(jediName(firstName, lastName));
+
+function beyond (num){
+    if(isFinite(num) !== true && num > 0){
+         console.log("To infinity");
+    }
+    else if (isFinite(num) !== true && num < 0){
+         console.log("to negative infinity");
+    }
+    else if (isFinite(num) == true) {
+         console.log("And Beyond");
+    }
+    else if (num === 0){
+         console.log("Staying home");
+    }
+}
+
+beyond (-Infinity);
