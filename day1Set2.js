f90f43bef20294886641194c7ddd96d84266d194
Brandon Graham <bgraham452@gmail.com>
2018-06-11 14:45:57 -0400

    added decode function

--- day1Set2.js
+++ day1Set2.js
@@ -26,3 +26,26 @@ function beyond (num){
 }
 
 beyond (-Infinity);
+
+
+function decode (word) {
+    
+    if (word.charAt(0) === 'a'){
+    return word.charAt(1);
+    }
+    else if (word.charAt(0) === 'b') {
+        return word.charAt(2);
+    }
+    else if (word.charAt(0) === 'c') {
+        return word.charAt(3);
+    }
+    else if (word.charAt(0) === 'd'){
+        return word.charAt(4);
+        }
+        else {
+            return ' ';
+        }
+}
+
+console.log (decode('craft') + decode('block') + decode('argon') + decode('meter') + decode ('bells') + decode('brown') 
++ decode('croon') + decode('droop'));
