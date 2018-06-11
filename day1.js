48da3a5a323b9453c6e8a95ab81ca7fa198ebd50
Brandon Graham <bgraham452@gmail.com>
2018-06-11 13:03:02 -0400

    Added params to whoAmI

--- day1.js
+++ day1.js
@@ -1,5 +1,7 @@
-function whoAmI (){
-    console.log('Hi, my name is Chris and I\'m 29 years old.');
+function whoAmI (name, age){
+    console.log(`Hi, my name is ${name} and I\'m ${age} years old.`);
 }
 
-whoAmI ();
+let name = "Rick";
+let age = 35;
+whoAmI (name, age);
