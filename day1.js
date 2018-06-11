e5c1753013a622aaab31c2f11080fd41b27c0c00
Ian Beihl <ianbeihl@gmail.com>
2018-06-11 10:07:54 -0700

    added yearOfBirth

--- day1.js
+++ day1.js
@@ -1,5 +1,8 @@
 function whoAmI (name, age){
+    let yearOfBirth = 2018 - age;
     console.log(`Hi, my name is ${name} and I\'m ${age} years old.`);
+    console.log(`I was born in ${yearOfBirth}`)
+
 }
 
 let name = "Rick";
